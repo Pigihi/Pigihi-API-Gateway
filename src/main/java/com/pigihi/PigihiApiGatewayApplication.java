8342e0565224f03ba8329d81b0f1091b920c68b7
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2023-02-02 10:15:55 +0530

    Setup directory structure

---
+++ src/main/java/com/pigihi/PigihiApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pigihi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigihiApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigihiApiGatewayApplication.class, args);
+	}
+
+}
