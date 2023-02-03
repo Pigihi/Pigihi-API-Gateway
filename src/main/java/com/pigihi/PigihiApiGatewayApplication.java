d02886991c80c172de00b694601425f97f1bfe2a
Ashish Sam T George <ashishsamtgeorge@mca.ajce.in>
2023-02-03 17:44:57 +0530

    Add microservices to API gateway
    * Customer
    * Order
    * Payment
    * Product
    * Service-Registry
    * Shop
    * User-Communication

--- src/main/java/com/pigihi/PigihiApiGatewayApplication.java
+++ src/main/java/com/pigihi/PigihiApiGatewayApplication.java
@@ -2,8 +2,11 @@ package com.pigihi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+// @EnableEurekaClient is not showing. I think this is not needed for the latest version
+@EnableDiscoveryClient
 public class PigihiApiGatewayApplication {
 
 	public static void main(String[] args) {
