f4c7382dfecbf96855dc40a7335bcbd0f26822a4
Ashish Sam <ashishsamtgeorge@mca.ajce.in>
2023-02-17 12:10:17 +0530

    Create docker-compose.yml

---
+++ src/main/java/com/pigihi/config/WebClientConfig.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.pigihi.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * @author Ashish Sam T George
+ *
+ */
+@Configuration
+public class WebClientConfig {
+	
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder loadBalancedWebClientBuilder(){
+		return WebClient.builder();
+	}
+
+}
